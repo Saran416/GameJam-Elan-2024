24c4112f4228e28f3ed8549f02b50f95e261a2a1
SPM <msoham2100@gmail.com>
2024-03-17 15:04:00 +0530

    Wallrun and platform bugfix(partial)

--- Assets/MovingScript.cs
+++ Assets/MovingScript.cs
@@ -8,6 +8,8 @@ public class MovingScript : MonoBehaviour
     public int currIndex = 0;
     public float speed = 2f;
 
+    public Vector3 currentVelocity;
+
     private void Update()
     {
         if (Vector3.Distance(transform.position, wavepoints[currIndex].transform.position) < 0.1f)
@@ -17,8 +19,17 @@ public class MovingScript : MonoBehaviour
                 currIndex = 0;
             }
         }
+        currentVelocity = (Vector3.MoveTowards(transform.position, wavepoints[currIndex].transform.position, speed * Time.deltaTime) - transform.position) / Time.deltaTime;
+
         transform.position = Vector3.MoveTowards(transform.position, wavepoints[currIndex].transform.position, speed*Time.deltaTime);
     }
 
-   
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "PlayerObj")
+        {
+            collision.gameObject.GetComponentInParent<Transform>().SetParent(transform);
+        }
+    }
+
 }
--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(transform);
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(null);
-        }
-    }
+ //   private void OnCollisionEnter(Collision collision)
+ //   {
+ //       if (collision.gameObject.name == "Player")
+ //       {
+ //           collision.gameObject.transform.SetParent(transform);
+ //       }
+ //   }
+ //
+ //   private void OnCollisionExit(Collision collision)
+ //   {
+ //       if (collision.gameObject.name == "Player")
+ //       {
+ //           collision.gameObject.transform.SetParent(null);
+ //       }
+ //   }
 }
