7ffe786102525ef8dbaff7d30b8cdb6c9801545d
SPM <msoham2100@gmail.com>
2024-03-18 01:54:31 +0530

    Fixed Audio in lvl1

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class AudioManager : MonoBehaviour
 {
@@ -11,18 +12,10 @@ public class AudioManager : MonoBehaviour
 
 	public Sound[] sounds;
 
+	private int currentindex = 0;
+
 	void Awake()
 	{
-		if (instance != null)
-		{
-			Destroy(gameObject);
-		}
-		else
-		{
-			instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-
 		foreach (Sound s in sounds)
 		{
 			s.source = gameObject.AddComponent<AudioSource>();
@@ -37,7 +30,20 @@ public class AudioManager : MonoBehaviour
 		Play("Theme");
 	}
 
-	public void Play(string sound)
+    private void FixedUpdate()
+    {
+        if (!sounds[currentindex].source.isPlaying)
+		{
+			currentindex = (currentindex + 1) % sounds.Length;
+
+            sounds[currentindex].source.volume = sounds[currentindex].volume * (1f + UnityEngine.Random.Range(-sounds[currentindex].volumeVariance / 2f, sounds[currentindex].volumeVariance / 2f));
+            sounds[currentindex].source.pitch = sounds[currentindex].pitch * (1f + UnityEngine.Random.Range(-sounds[currentindex].pitchVariance / 2f, sounds[currentindex].pitchVariance / 2f));
+
+            sounds[currentindex].source.Play();
+        }
+    }
+
+    public void Play(string sound)
 	{
 		Sound s = Array.Find(sounds, item => item.name == sound);
 		if (s == null)
